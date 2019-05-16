dc62fe9d6eabe80d135d65b2c4682d59dd2c620d
MaciejPradzynski <macpradz@gmail.com>
2019-05-16 20:47:32 +0200

    spring jdbc - custom row mapper

--- src/main/java/pl/macpradz/setup/repository/UserJdbcDao.java
+++ src/main/java/pl/macpradz/setup/repository/UserJdbcDao.java
@@ -1,10 +1,13 @@
 package pl.macpradz.setup.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import pl.macpradz.setup.entity.User;
@@ -16,6 +19,20 @@ public class UserJdbcDao {
 
     public List<User> findAll() {
         return jdbcTemplate.query("select * from users", new BeanPropertyRowMapper<>(User.class));
+    }
+
+    class PersonRowMapper implements RowMapper<User> {
+
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new User().setId(rs.getLong("id")).setLogin(rs.getString("login"))
+                    .setPassword(rs.getString("password")).setFirstName(rs.getString("first_name"))
+                    .setLastName(rs.getString("last_name"));
+        }
+    }
+
+    public List<User> findAllWithCustomRowMapper() {
+        return jdbcTemplate.query("select * from users", new PersonRowMapper());
     }
 
     public User findById(Long id) {
@@ -30,7 +47,7 @@ public class UserJdbcDao {
 
     public int insert(User user) {
         return jdbcTemplate.update("insert into users (login, password, first_name, last_name) values(?, ?, ?, ?)" ,
-                new Object[]{user.getLogin(), user.getPassword(), user.getFirstName(), user.getLastName()});
+                user.getLogin(), user.getPassword(), user.getFirstName(), user.getLastName());
     }
 
     public int update(User user) {
